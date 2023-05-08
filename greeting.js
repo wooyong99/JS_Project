2509c3ee6a985c59f75cfbfa652920aeb84c6e27
wooyong99 <wulovesk@naver.com>
2023-05-09 01:59:59 +0900

    파일 캐싱방지 코드 추가 및 greeting.js 작성

---
+++ greeting.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function saveName(text){
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(e){
+  e.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName(){
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text){
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerHTML=`Hello ! ${text}`;
+}
+
+function loadName(){
+  const currentUser = localStorage.getItem(USER_LS);
+  if(currentUser === null){
+    askForName();
+  }else{
+    paintGreeting(currentUser);
+  }
+}
+
+function init(){
+  loadName();
+}
+init();
+// 새로고침할 경우 임시로 값 삭제하는 코드
+localStorage.removeItem(USER_LS);
+
