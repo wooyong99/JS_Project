da22eb6546c92af83a2efeab95564faa3e66ea88
wooyong99 <wulovesk@naver.com>
2023-05-09 20:06:14 +0900

    날씨 이미지 추가 및 css 변경

--- weather.js
+++ weather.js
@@ -4,14 +4,48 @@ const API_KEY = '3d118924307041340ab8ee58083a920e';
 
 const COORDS = 'coords';
 
+function getWeatherImg(weather){
+  const today_icon = document.querySelector(".today-icon");
+  const img = new Image();
+    img.src = `images/${weather}.png`;
+    today_icon.appendChild(img);
+}
+
 function getWeather(lat, lon){
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     ).then(function(response) {
         return response.json();
     }).then(json=>{
         console.log(json);
-        const place = json.name;
+        const weather = json.weather[0].main;
+        console.log(weather);
+        getWeatherImg(weather);
+
         const temperature = json.main.temp;
+        const temp = document.querySelector(".temp");
+        temp.innerHTML = `${temperature}`;
+
+        const max_temp = json.main.temp_max;
+        const max = document.querySelector(".max");
+        max.innerHTML = `${max_temp}`;
+
+        const min_temp = json.main.temp_min;
+        const min = document.querySelector(".min");
+        min.innerHTML = `${min_temp}`;
+
+        const wind_speed = json.wind.speed;
+        const wind = document.querySelector(".wind");
+        wind.innerHTML = `${wind_speed}`;
+
+        const clouds= json.clouds.all;
+        const cloud = document.querySelector(".clouds");
+        cloud.innerHTML = `${clouds}`;
+
+        const city = json.name;
+        const place = document.querySelector(".city");
+        place.innerHTML = `${city}`;
+        
+        
         weather.innerText = `${temperature} @ ${place}`;
     });
 }
